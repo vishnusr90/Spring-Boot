220939a9b487c513754564219e880f3f7f7313f1
Vishnu <vishnu.rajasree@lifefitness.com>
2018-04-15 09:37:14 +0800

    Added encryption to application.properties - Jasypt

--- src/main/java/com/springboot/learn/Main.java
+++ src/main/java/com/springboot/learn/Main.java
@@ -3,7 +3,10 @@ package com.springboot.learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Main {
 	
 	public static void main(String args[]){
